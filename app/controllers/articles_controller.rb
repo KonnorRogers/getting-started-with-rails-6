ba2c0a074f8aca3acac9bb2bdc8e1b50bf0e0c34
ParamagicDev <konnor5456@gmail.com>
2020-05-12 14:31:27 -0400

    fixing stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   def new; end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to @article
   end
 end
-
