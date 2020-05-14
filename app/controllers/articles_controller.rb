40831d92bed4582e5aa0d91f27c2b2f807170524
paramagicdev <konnor5456@gmail.com>
2020-05-13 23:40:18 -0400

    finished @ section 6, Adding a second model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,58 @@
 # frozen_string_literal: true
 
+# Articles
 class ArticlesController < ApplicationController
-  def new; end
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = @articles || Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
-    @article = Article.new(params[:article])
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# Article Model
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
