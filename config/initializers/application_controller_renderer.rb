5c70caa14c62e4c1f2dbb2e5a610174acbb9bfbb
ParamagicDev <konnor5456@gmail.com>
2020-09-10 14:49:01 -0400

    remove test stuff

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
+#   ApplicationController.renderer.for(self, {http_host: "localhost"}, ActionController.defaults.merge!(http_host: "localhost"))
+# end
+
 # ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
+#   ActionController::Renderer.for(ApplicationController, {http_host: "localhost"}, http_host: "localhost")
 # end
